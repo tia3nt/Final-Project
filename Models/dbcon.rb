204ab8c3190a0d4604ff2d02d04dbb6a3868fc36
tia3nt <tia3nt@gmail.com>
2021-08-15 01:50:14 +0700

    To Check wether or not prepared data already exist in previously inserted database, some database mock testing has also beeing repaired
    Signed-off-by: tia3nt <tia3nt@gmail.com>

--- Models/dbcon.rb
+++ Models/dbcon.rb
@@ -1,3 +1,4 @@
+
 class Db_Conn
   def self.connect
     client = Mysql2::Client.new(
@@ -14,11 +15,54 @@ class Db_Conn
   end
 
   def self.query(query_line)
-    rawData = @@client.query(query_line)
+    rawData = self.query_only(query_line)
     lists = Array.new
     rawData.each do |list|
       lists.push(list)
     end
     lists
+  end
+
+  def self.create(table, col, data)
+    column_text = ""
+    col.each do |list|
+      column_text << list
+      column_text << ", "
+    end
+    column_text=column_text[0..-3]
+
+    data_text = ""
+    data.each do |lists|
+      data_text << "("
+      lists.each do |list|
+        data_text << '"'
+        data_text << list
+        data_text << '", '
+      end
+      data_text=data_text[0..-3]
+      data_text << "), "
+    end
+    data_text=data_text[0..-3]
+    
+    @@client.query("
+      INSERT INTO #{table}
+      (#{column_text})
+      VALUES #{data_text}
+      ")
+  end
+
+  def self.exist?(table, param)
+    condition = ""
+    param.each do
+      |key, value|
+      condition << "#{key} = '#{value}' OR "
+    end
+    condition = condition[0..-5]
+
+    rawData = @@client.query("
+          select * from #{table} where #{condition}")
+    check = rawData.count
+
+    return check>0
   end
 end
--- spec/MyTwitt_spec.rb
+++ spec/MyTwitt_spec.rb
@@ -6,7 +6,7 @@ require 'rack/test'
 
 
 
-set :environment, :test_helper
+set :environment, :test
 
 
 def app
@@ -14,15 +14,52 @@ def app
 end
 
 
+
 RSpec.describe 'MyTwitt' do
+
   describe Db_Conn do
       context 'When real database accept queries' do
-        it 'mocks database should be abble to take over on test' do
-            db_conn_mock = double
-            allow(Mysql2::Client).to receive(:new).and_return(db_conn_mock)
-                data = Db_Conn.query_only('SHOW TABLES')
-            expect(Mysql2::Result).to be_truthy
+        before :each do
+          Db_Mock = class_double(Db_Conn)
+        end
 
+        it 'mocks database should be abble to take over on test' do
+            Db_Mock.as_stubbed_const
+            expect(Db_Mock).to receive(:query_only).once
+            Db_Conn.query_only('Show tables')
+        end
+
+        context 'When given valid input' do
+          it 'should be able to create new database records' do
+            table = 'tbl_user'
+            table_columns = ["user_name", "user_email", "user_password", "user_bio"]
+            table_values =[["Vanita Rania", "vanita@rania.net", "pass1", "1995-03-20"],
+                            ["Meriam Wiranata", "Mwira@gmail.com", "pass2", "2000-01-23"]]
+
+            Db_Mock.as_stubbed_const
+            expect(Db_Mock).to receive(:create).with(table, table_columns, table_values)
+              Db_Conn.create(table, table_columns, table_values)
+
+
+          end
+
+          it 'should be able to detect existing data' do
+            table = "tbl_user"
+            prev_col = ["user_name", "user_email", "user_password", "user_bio"]
+            prev_data =[["Vanita Rania", "vanita@rania.net", "pass1", "1995-03-20"],
+                        ["Meriam Wiranata", "Mwira@gmail.com", "pass2", "2000-01-23"]]
+
+            Db_Conn.create(table, prev_col, prev_data)
+
+            new_data = {:user_id => 1, :user_email => "Mwira@gmail.com"}
+
+            expect(Db_Conn).to receive(:exist?).with(table, new_data).and_call_original
+
+            check = Db_Conn.exist?(table, new_data)
+            expect(check).to be_truthy
+            Db_Conn.query_only("Delete from tbl_user")
+
+          end
         end
 
       end
@@ -31,8 +68,7 @@ RSpec.describe 'MyTwitt' do
 
   describe 'sinatra running' do
     include Rack::Test::Methods
-    
-
+  #
     it 'should be abble to handle query and automatically handle rawdata into sinatra previewed-able' do
       db_conn_mock = double
       allow(Mysql2::Client).to receive(:new).and_return(db_conn_mock)
@@ -42,6 +78,13 @@ RSpec.describe 'MyTwitt' do
     end
 
   end
+
+  # after :all do
+  #   allow(Db_Conn).to receive(:query_only).with("Delete From tbl_user")
+  #   allow(Db_Conn).to receive(:query_only).with("Delete From tbl_collections")
+  #   allow(Db_Conn).to receive(:query_only).with("Delete From tbl_comments")
+  #   allow(Db_Conn).to receive(:query_only).with("Delete From tbl_hashtag")
+  # end
 
 end
 
