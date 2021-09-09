3856f79ad8baeaefd26d76265a2e8b3a27fef35f
tia3nt <tia3nt@gmail.com>
2021-09-09 16:00:29 +0700

    Refactoring code smeel based on rubocop on app.rb
    Signed-off-by: tia3nt <tia3nt@gmail.com>

--- Models/db_conn.rb
+++ Models/db_conn.rb
@@ -1,53 +1,47 @@
-
 class Db_Conn
-  @@idset= {  "tbl_user"=> "user_id",
-              "tbl_collections" => "collection_id",
-              "tbl_comments" => "comment_id",
-              "tbl_hashtag" => "hash_id"}
+  @@idset = {
+    'tbl_user' => 'user_id',
+    'tbl_collections' => 'collection_id',
+    'tbl_comments' => 'comment_id',
+    'tbl_hashtag' => 'hash_id'
+  }
   def self.connect
-    client = Mysql2::Client.new(
-      :host => 'localhost',
-      :username => ENV['DB_Username'],
-      :password => ENV['DB_Password'],
-      :database => ENV['DB_Final']
+    Mysql2::Client.new(
+      'host' => 'localhost',
+      'username' => ENV['DB_Username'],
+      'password' => ENV['DB_Password'],
+      'database' => ENV['DB_Final']
     )
   end
-
-
+  @client = self.connect
   def self.query_only(query_line)
-    client = self.connect
-    rawData = client.query(query_line)
+    rawData = @client.query(query_line)
   end
-
   def self.data_to_object(rawdata)
-       lists = Array.new
-       rawdata.each do
-         |list|
-         lists << list
-       end
-       lists
+    lists = Array.new
+    rawdata.each do
+      |list|
+      lists << list
+    end
+    lists
   end
-
-
   def self.create(table, header, data)
-
-    column_text = ""
+    column_text = ''
     header.each do |list|
       column_text << list
-      column_text << ", "
+      column_text << ', '
     end
-
     column_text=column_text[0..-3]
-    data_text = ""
+    data_text = ''
     data.each do |lists|
-      data_text << "("
+      data_text << '('
       lists.each do |list|
         data_text << '"'
         data_text << list
         data_text << '", '
       end
       data_text=data_text[0..-3]
-      data_text << "), "
+      data_text << '), '
     end
     data_text=data_text[0..-3]
     self.query_only("
@@ -56,73 +50,61 @@ class Db_Conn
       VALUES #{data_text}
       ")
   end
-
   def self.exist?(table, param)
-    condition = ""
+    condition = ''
     param.each do
       |key, value|
       condition << "#{key} = '#{value}' OR "
     end
     condition = condition[0..-5]
-
     rawData = self.query_only("
-          select * from #{table} where #{condition}")
+      select * from #{table} where #{condition}")
     check = rawData.count
-
-    return check>0
+    return check > 0
   end
-
-def self.find_id(table, data, operand)
-
-idset = @@idset["#{table}"]
-conditions =""
-length = (operand.size + 2)*-1
+  def self.find_id(table, data, operand)
+    idset = @@idset["#{table}"]
+    conditions = ''
+    length = (operand.size + 2) * -1
     data.each do
       |key, value|
-      if value.include?("%")
+      if value.include?('%')
         conditions << "#{key} LIKE '#{value}' #{operand} "
       else
         conditions << "#{key} = '#{value}' #{operand} "
       end
     end
-  conditions = conditions[0..length]
-
-  rawData = self.query_only("
-    select #{idset} from #{table} where #{conditions}")
+    conditions = conditions[0..length]
+    rawData = self.query_only("
+      select #{idset} from #{table} where #{conditions}")
     return rawData.each
-end
-
-def self.edit(table, new_data, id)
-  idset = @@idset["#{table}"]
-  toset= ""
-  new_data.each do
-    |key, value|
-    toset << "#{key} = '#{value}', "
   end
-  toset = toset[0..-3]
-
-  self.query_only("
+  def self.edit(table, new_data, id)
+    idset = @@idset["#{table}"]
+    toset= ''
+    new_data.each do
+      |key, value|
+      toset << "#{key} = '#{value}', "
+    end
+    toset = toset[0..-3]
+    self.query_only("
       UPDATE #{table}
       SET #{toset}
       WHERE #{idset} = #{id}
-      ")
-end
-
-
-def self.delete(table, parameter, operand)
-  condition = ""
-  length = (operand.size + 2)* -1
-  parameter.each do |key, value|
-    if value.to_s.include?("%")
+    ")
+  end
+  def self.delete(table, parameter, operand)
+    condition = ''
+    length = (operand.size + 2) * -1
+    parameter.each do |key, value|
+      if value.to_s.include?('%')
         condition << "#{key} LIKE '#{value}' #{operand} "
       else
         condition << "#{key} = '#{value}' #{operand} "
+      end
     end
+    condition = condition[0..length]
+    self.query_only("
+      DELETE FROM #{table} WHERE #{condition}")
   end
-  condition = condition[0..length]
-
-  self.query_only("
-    DELETE FROM #{table} WHERE #{condition}")
-end
-
 end
--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'mysql2'
 require 'fileutils'
@@ -26,118 +28,105 @@ set :port, '2222'
 PUBLIC_DIR = File.join(File.dirname(__FILE__), 'static')
 
 class App < Sinatra::Base
-sinatra_flag = 'start'
-
- get '/signup' do
-   Controller_main.showERB('signup.erb')
- end
-
- post '/signup' do
-   content_type :json
-   active_user = Control_user.new(params)
-   active_user.signup
-   Db_Conn.query_only("select * from tbl_user").to_json
- end
-
- get '/login' do
-   Controller_main.showERB('login.erb')
- end
-
- post '/login' do
-   content_type :json
-   input_user = {
-     "user_email" => params["user_email"],
-     "user_password" => params["user_password"]
-   }
-   user_id = User.get_id_by_parameter(input_user)
-   active_user = User.get_by_id(user_id)
-   active_user = User.new(active_user)
-   return false unless active_user.getter("user_password") == params["user_password"]
-   active_user.to_json
-   redirect "/user/#{user_id}"
- end
-
-get '/user/:id?' do
-  content_type :json
-  user_belongging = Hash.new
-  active_user = User.get_by_id(params["id"])
-  history = Collection.belong_to(params["id"])
-  user_belongging = {
-    "user_information" => active_user,
-    "user_collections_list" => history}
-  user_belongging.to_json
-end
-
-post '/new/collection/user/:user_id' do
-  content_type :json
-  data_input = Collection.new(params)
-  new_collection = data_input.create
-  new_collection.to_json
-end
-
-get "/find/user/:name" do
-  content_type :json
-  value = params["name"]
-  id_list = User.get_id_by_name_like(value)
-  "There is no user with that name" if id_list.each.nil?
-  datas_found = Array.new
-  data_found = Hash.new
-  id_list.each do |list|
-    user = User.get_by_id(list)
-    data = Collection.belong_to(list)
-    data_found = {"user_information" => user, "user_collections_list" => data}
-    datas_found << data_found
+  get '/signup' do
+    Controller_main.showERB('signup.erb')
   end
-  datas_found.to_json
-end
+  post '/signup' do
+    content_type :json
+    active_user = Control_user.new(params)
+    active_user.signup
+    Db_Conn.query_only('select * from tbl_user').to_json
+  end
+  get '/login' do
+    Controller_main.showERB('login.erb')
+  end
+  post '/login' do
+    content_type :json
+    input_user = {
+      'user_email' => params['user_email'],
+      'user_password' => params['user_password']
+    }
+    user_id = User.get_id_by_parameter(input_user)
+    active_user = User.get_by_id(user_id)
+    active_user = User.new(active_user)
+    return false unless active_user.getter('user_password') == params['user_password']
 
-post "/post/comment/user/:user_id/collection/:collection_id" do
-  content_type :json
-  params.store("post_id", "#{params["collection_id"]}")
-  new_data = Comment.new(params)
-  records = new_data.create
-  records.to_json
-end
-
-get "/find/hashtag/:text" do
-  content_type :json
-  text_to_find = params["text"].downcase
-  result = Hashtag.find(text_to_find)
-  result.to_json
-end
-
-get "/hashtags/trending" do
-  content_type :json
-  result = Hashtag.topfive
-  result.to_json
-end
-get "/attach/collection/gallery" do
-  last_collection_id = Collection.last_record
-  renderer = ERB.new(File.read("./views/gallery.erb"))
-  renderer.result(binding)
-end
-
-post "/attach/collection/picture/:collection_id" do
-  content_type :json
-  gallery = Picture_Gallery.new(params)
-  gallery.upload
-  updated_data = Collection.get_by_id(params["collection_id"])
-  updated_data.to_json
-end
-
-post "/attach/collection/video/:collection_id" do
-  content_type :json
-  gallery = Video_Gallery.new(params)
-  gallery.upload
-  updated_data = Collection.get_by_id(params["collection_id"])
-  updated_data.to_json
-end
-
-post "/attach/collection/file/:collection_id" do
-  content_type :json
-  gallery = Gallery.new(params)
-  gallery.upload
-  updated_data = Collection.get_by_id(params["collection_id"])
-  updated_data.to_json
-end
+    active_user.to_json
+    redirect "/user/#{user_id}"
+  end
+  get '/user/:id?' do
+    content_type :json
+    active_user = User.get_by_id(params['id'])
+    history = Collection.belong_to(params['id'])
+    user_belongging = {
+      'user_information' => active_user,
+      'user_collections_list' => history
+    }
+    user_belongging.to_json
+  end
+  post '/new/collection/user/:user_id' do
+    content_type :json
+    data_input = Collection.new(params)
+    new_collection = data_input.create
+    new_collection.to_json
+  end
+  get '/find/user/:name' do
+    content_type :json
+    value = params['name']
+    id_list = User.get_id_by_name_like(value)
+    'There is no user with that name' if id_list.each.nil?
+    datas_found = []
+    data_found = {}
+    id_list.each do |list|
+      user = User.get_by_id(list)
+      data = Collection.belong_to(list)
+      data_found = { 'user_information' => user, 'user_collections_list' => data }
+      datas_found << data_found
+    end
+    datas_found.to_json
+  end
+  post '/post/comment/user/:user_id/collection/:collection_id' do
+    content_type :json
+    params.store('post_id', params['collection_id'].to_s)
+    new_data = Comment.new(params)
+    records = new_data.create
+    records.to_json
+  end
+  get '/find/hashtag/:text' do
+    content_type :json
+    text_to_find = params['text'].downcase
+    result = Hashtag.find(text_to_find)
+    result.to_json
+  end
+  get '/hashtags/trending' do
+    content_type :json
+    result = Hashtag.topfive
+    result.to_json
+  end
+  get '/attach/collection/gallery' do
+    last_collection_id = Collection.last_record
+    renderer = ERB.new(File.read('./views/gallery.erb'))
+    renderer.result(binding)
+  end
+  post '/attach/collection/picture/:collection_id' do
+    content_type :json
+    gallery = Picture_Gallery.new(params)
+    gallery.upload
+    updated_data = Collection.get_by_id(params['collection_id'])
+    updated_data.to_json
+  end
+  post '/attach/collection/video/:collection_id' do
+    content_type :json
+    gallery = Video_Gallery.new(params)
+    gallery.upload
+    updated_data = Collection.get_by_id(params['collection_id'])
+    updated_data.to_json
+  end
+  post '/attach/collection/file/:collection_id' do
+    content_type :json
+    gallery = Gallery.new(params)
+    gallery.upload
+    updated_data = Collection.get_by_id(params['collection_id'])
+    updated_data.to_json
+  end
 end
